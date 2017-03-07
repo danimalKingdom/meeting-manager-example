581e958f19623946199e61b194d8a2447e57d3e0
dani <dani.zaghian@gmail.com>
2017-03-06 21:19:08 -0800

    added button and function to sort by date

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -2,11 +2,21 @@
   "use strict";
 
   angular.module("app").controller('meetingsCtrl', function($scope, $http){
-    $scope.message = "hello world";
+
     $scope.setUp = function(){
       $http.get('/api/v1/meetings.json').then(function(response){
         $scope.meetings = response.data;
       });
+    };
+
+    $scope.changeOrderAttribute = function(attribute){
+      if(attribute !== $scope.orderAttribute){
+        $scope.orderDescending = false;
+      } else {
+        $scope.orderDescending = !$scope.orderDescending;
+      }
+
+      $scope.orderAttribute = attribute;
     };
   });
 
