145f9acfcaa55c9123f9edd0ce4af0e63eac4149
dani <dani.zaghian@gmail.com>
2017-03-06 20:58:12 -0800

    rewrote index in angular, added tags api

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,8 +1,13 @@
 (function() {
   "use strict";
 
-  angular.module("app").controller('meetingsCtrl', function($scope){
+  angular.module("app").controller('meetingsCtrl', function($scope, $http){
     $scope.message = "hello world";
+    $scope.setUp = function(){
+      $http.get('/api/v1/meetings.json').then(function(response){
+        $scope.meetings = response.data;
+      });
+    };
   });
 
 }());
