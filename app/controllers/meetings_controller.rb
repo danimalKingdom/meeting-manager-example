c963ccf7220bfd5f6ce957158d32512f820f8e66
dani <dani.zaghian@gmail.com>
2017-03-06 18:47:26 -0800

    added new and create routes for meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,6 +8,34 @@ class MeetingsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    @meeting = Meeting.new
+    @tags = Tag.all # array of all our tags
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+      )
+    if @meeting.save
+      params[:tags].each do |tag_id| #array of tag ids
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+          )
+      end
+      flash[:success] = "Meeting successfully created"
+      redirect_to "/meetings/#{@meeting.id}"
+    else
+      @tags = Tag.all
+      render :new
+    end
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
     render 'show.html.erb'
