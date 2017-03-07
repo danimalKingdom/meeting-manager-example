0b58f251a23d3a009d009ccd0214a177523e7e00
dani <dani.zaghian@gmail.com>
2017-03-06 20:18:50 -0800

    meetings api namespaced routes, index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   get '/meetings/:id/edit' => 'meetings#edit'
   patch '/meetings/:id' => 'meetings#update'
 
+  namespace :api do
+    namespace :v1 do
+      get '/meetings' => 'meetings#index'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
